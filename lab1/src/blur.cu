6729f02ef1392e306ade1bd345709788f2c4f262
raytroop <raytroop@gmail.com>
2019-10-18 11:37:28 +0800

    lab1 done

--- lab1/src/blur.cu
+++ lab1/src/blur.cu
@@ -1,6 +1,6 @@
-/* 
+/*
  * CUDA blur
- * Kevin Yuh, 2014 
+ * Kevin Yuh, 2014
  * Revised by Nailen Matschke, 2016
  * Revised by Loko Kung, 2018
  */
@@ -13,29 +13,42 @@
 #include "cuda_header.cuh"
 
 CUDA_CALLABLE
-void cuda_blur_kernel_convolution(uint thread_index, const float* gpu_raw_data,
+void cuda_blur_kernel_convolution(unsigned int thread_index, const float* gpu_raw_data,
                                   const float* gpu_blur_v, float* gpu_out_data,
                                   const unsigned int n_frames,
                                   const unsigned int blur_v_size) {
     // TODO: Implement the necessary convolution function that should be
     //       completed for each thread_index. Use the CPU implementation in
     //       blur.cpp as a reference.
+    if (thread_index < blur_v_size)
+    {
+        for (int j = 0; j <= thread_index; j++)
+        {
+            gpu_out_data[thread_index] += gpu_raw_data[thread_index - j] * gpu_blur_v[j];
+        }
+    } else {
+        for (int j = 0; j < blur_v_size; j++)
+        {
+            gpu_out_data[thread_index] += gpu_raw_data[thread_index - j] * gpu_blur_v[j];
+        }
+    }
 }
 
 __global__
 void cuda_blur_kernel(const float *gpu_raw_data, const float *gpu_blur_v,
                       float *gpu_out_data, int n_frames, int blur_v_size) {
     // TODO: Compute the current thread index.
-    uint thread_index;
-
+    unsigned int thread_index;
+    thread_index = blockIdx.x * blockDim.x + threadIdx.x;
     // TODO: Update the while loop to handle all indices for this thread.
     //       Remember to advance the index as necessary.
-    while (false) {
+    while (thread_index < n_frames) {
         // Do computation for this thread index
         cuda_blur_kernel_convolution(thread_index, gpu_raw_data,
                                      gpu_blur_v, gpu_out_data,
                                      n_frames, blur_v_size);
         // TODO: Update the thread index
+        thread_index += gridDim.x * blockDim.x;
     }
 }
 
@@ -59,20 +72,28 @@ float cuda_call_blur_kernel(const unsigned int blocks,
     //       has n_frames elements. Then copy the data in raw_data into the
     //       GPU memory you allocated.
     float* gpu_raw_data;
+    cudaMalloc((void **)&gpu_raw_data, n_frames * sizeof(float));
+    cudaMemcpy(gpu_raw_data, raw_data, n_frames * sizeof(float),
+               cudaMemcpyHostToDevice);
 
     // TODO: Allocate GPU memory for the impulse signal (for now global GPU
     //       memory is fine. The data is of type float and has blur_v_size
     //       elements. Then copy the data in blur_v into the GPU memory you
     //       allocated.
     float* gpu_blur_v;
+    cudaMalloc((void **)&gpu_blur_v, blur_v_size * sizeof(float));
+    cudaMemcpy(gpu_blur_v, blur_v, blur_v_size * sizeof(float),
+               cudaMemcpyHostToDevice);
 
     // TODO: Allocate GPU memory to store the output audio signal after the
     //       convolution. The data is of type float and has n_frames elements.
     //       Initialize the data as necessary.
     float* gpu_out_data;
-    
-    // TODO: Appropriately call the kernel function.
+    cudaMalloc((void **) &gpu_out_data, n_frames * sizeof(float));
+    cudaMemset(gpu_out_data, 0, n_frames * sizeof(float));
 
+    // TODO: Appropriately call the kernel function.
+    cuda_blur_kernel<<<blocks, threads_per_block>>>(gpu_raw_data, gpu_blur_v, gpu_out_data, n_frames, blur_v_size);
     // Check for errors on kernel call
     cudaError err = cudaGetLastError();
     if (cudaSuccess != err)
@@ -83,10 +104,12 @@ float cuda_call_blur_kernel(const unsigned int blocks,
     // TODO: Now that kernel calls have finished, copy the output signal
     //       back from the GPU to host memory. (We store this channel's result
     //       in out_data on the host.)
-
+    cudaMemcpy(out_data, gpu_out_data, n_frames * sizeof(float), cudaMemcpyDeviceToHost);
     // TODO: Now that we have finished our computations on the GPU, free the
     //       GPU resources.
-
+    cudaFree(gpu_raw_data);
+    cudaFree(gpu_blur_v);
+    cudaFree(gpu_out_data);
     // Stop the recording timer and return the computation time
     cudaEventRecord(stop_gpu);
     cudaEventSynchronize(stop_gpu);
